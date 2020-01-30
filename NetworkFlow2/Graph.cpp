e1362715b336fd60b8c856c7478c90a33ae83200
d3vv3d <d3V.v3d@gmail.com>
2020-01-30 08:40:21 -0800

    Removed some code that was commented out.

--- NetworkFlow2/Graph.cpp
+++ NetworkFlow2/Graph.cpp
@@ -133,39 +133,6 @@ unsigned int Graph::maxFlow() {
 	return minCut;
 }
 
-// Willnot always find a path to t if there is one, needs a dead end case, also this doe not preventy cycles, whoops
-// Do not add edges to result unitl 't' has been reached, also
-// bool Graph::depthFirstSearch(char root, std::vector
-// Broken first attempt
-/*
-std::vector<Edge> Graph::depthFirstSearch(char root) {
-	std::vector<Edge> result;
-	
-	for (unsigned int i = 0; i < m_edges.size(); i++) {
-		if (m_edges[i].m_head == root && m_edges[i].m_flow < m_edges[i].m_capacity) {
-			result.push_back(m_edges[i]);
-			std::vector<Edge> tmp = depthFirstSearch(m_edges[i].m_tail);
-			result.insert(result.end(), tmp.begin(), tmp.end());
-		}
-	}
-
-	if (result.back().m_tail == 't') {
-		return result;
-	}
-
-	for (unsigned int i = 0; i < m_edges.size(); i++) {
-		if (m_edges[i].m_tail == root && m_edges[i].m_flow > 0) {
-			result.push_back(m_edges[i]);
-			std::vector<Edge> tmp = depthFirstSearch(m_edges[i].m_head);
-			result.insert(result.end(), tmp.begin(), tmp.end());
-		}
-	}
-
-	return result;
-
-}
-*/
-
 std::ostream& operator<<(std::ostream& os, const Graph& graph) {
 	std::vector<Edge>::const_iterator iter;
 
